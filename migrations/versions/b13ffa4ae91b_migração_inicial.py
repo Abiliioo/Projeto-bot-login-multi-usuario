2851abfec8a39a84ce1abc55e10e201389cbcb8d
abiliioo <joseiltonjass@hotmail.com>
2024-10-08 12:42:00 -0300

    Corrigir tamanho do campo password_hash

--- migrations/versions/b13ffa4ae91b_migração_inicial.py
+++ migrations/versions/b13ffa4ae91b_migração_inicial.py
@@ -32,6 +32,7 @@ def upgrade():
     with op.batch_alter_table('users', schema=None) as batch_op:
         batch_op.create_index(batch_op.f('ix_users_email'), ['email'], unique=True)
         batch_op.create_index(batch_op.f('ix_users_username'), ['username'], unique=True)
+        batch_op.alter_column('password_hash', type_=sa.String(length=512), nullable=False)
 
     op.create_table('keywords',
     sa.Column('id', sa.Integer(), nullable=False),
